327e76b6f447cafaa9b888336b119384c7d58d92
Ivan S. <mainpostskvorcov@gmail.com>
2024-12-14 18:07:16 +0300

    Trying to fix autotests

--- lab0/basic_math.py
+++ lab0/basic_math.py
@@ -1,29 +1,26 @@
 import numpy as np
-import math
+import scipy as sc
 
 
 def matrix_multiplication(matrix_a, matrix_b):
     if len(matrix_a[0]) != len(matrix_b):
-        raise ValueError("Количество столбцов первой матрицы не равно числу строк во второй матрицы.")
-    
+        raise ValueError("Количество столбцов первой матрицы должно быть равно количеству строк второй матрицы")
     result = [[0 for _ in range(len(matrix_b[0]))] for _ in range(len(matrix_a))]
-    
-    for i in range(len(matrix_a)): 
-        for k in range(len(matrix_b[0])): 
-            for j in range(len(matrix_b)):  
-                result[i][k] += matrix_a[i][j] * matrix_b[j][k]
-    
+    for i in range(len(matrix_a)):
+        for j in range(len(matrix_b[0])):
+            for k in range(len(matrix_b)):
+                result[i][j] += matrix_a[i][k] * matrix_b[k][j]
     return result
 
 
-def find_extremum(a, b):
-    if a == 0:
-        return None
-    return -b / (2 * a)
+def functions(a_1, a_2):
+    def find_extremum(a, b):
+        if a == 0:
+            return None
+        return -b/(2*a)
 
-def functions(a_1, a_2):    
-    a11, a12, a13 = map(int, a_1.split())
-    a21, a22, a23 = map(int, a_2.split())
+    a11, a12, a13 = list(map(int, a_1.split())) 
+    a21, a22, a23 = list(map(int, a_2.split()))
 
     extrema1 = find_extremum(a11, a12)
     extrema2 = find_extremum(a21, a22)
@@ -31,56 +28,35 @@ def functions(a_1, a_2):
     a = a11 - a21
     b = a12 - a22
     c = a13 - a23
-
-    if a == 0 and b == 0 and c == 0:
-        return None  # Бесконечное количество решений
-
+    if a == 0 and b == 0 and c==0:
+        return None
+    if a == 0 and b == 0:
+       return []
     if a == 0:
-        if b == 0:
-            return []  # Нет решений
-        x = -c / b  # Линейное уравнение
-        return [(x, a11 * x**2 + a12 * x + a13)]
+       x = -c/b
+       return [(x, a11 * x**2 + a12 * x + a13)]
 
-    discriminant = b**2 - 4 * a * c
-
-    if discriminant > 0:
-        sqrt_d = math.sqrt(discriminant)
-        x1 = (-b + sqrt_d) / (2 * a)
-        x2 = (-b - sqrt_d) / (2 * a)
+    d = b*b - 4*a*c
+    if d > 0:
+        x1 = (-b + np.sqrt(d)) / (2*a)
+        x2 = (-b - np.sqrt(d)) / (2*a)
         return [(x1, a11 * x1**2 + a12 * x1 + a13), (x2, a11 * x2**2 + a12 * x2 + a13)]
-    
-    if discriminant == 0:
-        x = -b / (2 * a)
+    if d == 0:
+        x = -b/(2*a)
         return [(x, a11 * x**2 + a12 * x + a13)]
-    
-    return []  # Нет реальных решений (дискриминант < 0)
+    return []
+
 
 def skew(x):
-    n = len(x)
-    
-    mean_x = sum(x) / n
-    
-    variance = sum((xi - mean_x) ** 2 for xi in x) / n
-    std_x = variance ** 0.5
-    
-    m3 = sum((xi - mean_x) ** 3 for xi in x) / n
-    
+    mean_x = np.mean(x)
+    std_x = np.std(x, ddof=0)
+    m3 = np.mean((x - mean_x)**3)
     skewness = m3 / std_x**3
-    
     return round(skewness, 2)
 
-
 def kurtosis(x):
-    n = len(x)
-    
-    mean_x = sum(x) / n
-    
-    variance = sum((xi - mean_x) ** 2 for xi in x) / n
-    std_x = variance ** 0.5
-    
-    m4 = sum((xi - mean_x) ** 4 for xi in x) / n
-    
-    # Коэффициент эксцесса
+    mean_x = np.mean(x)
+    std_x = np.std(x, ddof=0)
+    m4 = np.mean((x - mean_x)**4)
     kurt = m4 / std_x**4 - 3
-    
     return round(kurt, 2)
